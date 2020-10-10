dd0b9b7ba5b06aabf9baa315c1b0198d1739c084
Jean Bisutti <jean.bisutti@gmail.com>
2020-10-10 15:12:28 +0200

    Add example

---
+++ src/main/java/org/quickperf/micronaut/micronauttest/service/PlayerService.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2020-2020 the original author or authors.
+ */
+
+package org.quickperf.micronaut.micronauttest.service;
+
+import org.quickperf.micronaut.micronauttest.dto.PlayerWithTeamName;
+import org.quickperf.micronaut.micronauttest.jdbc.entity.Player;
+import org.quickperf.micronaut.micronauttest.jdbc.entity.Team;
+import org.quickperf.micronaut.micronauttest.jdbc.repository.PlayerRepository;
+import org.quickperf.micronaut.micronauttest.jdbc.repository.TeamRepository;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Singleton
+public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    private final TeamRepository teamRepository;
+
+    public PlayerService(PlayerRepository playerRepository, TeamRepository teamRepository) {
+        this.playerRepository = playerRepository;
+        this.teamRepository = teamRepository;
+    }
+
+    public List<PlayerWithTeamName> findPlayersWithTeamName() {
+
+        Iterable<Player> players = playerRepository.findAll();
+
+        List<PlayerWithTeamName> playersWithTeamName = new ArrayList<>();
+        for (Player aPlayer : players) {
+            Team team = aPlayer.getTeam();
+            Long teamId = team.getId();
+            System.out.println("teamId = " + teamId);
+            String teamName = team.getName();
+            System.out.println("teamName = " + teamName);
+            PlayerWithTeamName playerWithTeamName
+                    = new PlayerWithTeamName(aPlayer.getFirstName()
+                                           , aPlayer.getLastName()
+                                           , teamName);
+            playersWithTeamName.add(playerWithTeamName);
+        }
+
+        return playersWithTeamName;
+
+    }
+
+}
---
+++ src/test/java/org/quickperf/micronaut/micronauttest/service/PlayerServiceTest.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2020-2020 the original author or authors.
+ */
+
+package org.quickperf.micronaut.micronauttest.service;
+
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.quickperf.junit5.QuickPerfTest;
+import org.quickperf.micronaut.micronauttest.dto.PlayerWithTeamName;
+import org.quickperf.micronaut.micronauttest.jdbc.entity.Player;
+import org.quickperf.micronaut.micronauttest.jdbc.entity.Team;
+import org.quickperf.micronaut.micronauttest.jdbc.repository.PlayerRepository;
+import org.quickperf.micronaut.micronauttest.jdbc.repository.TeamRepository;
+import org.quickperf.sql.annotation.DisplaySql;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@MicronautTest
+@QuickPerfTest
+public class PlayerServiceTest {
+
+    @Inject
+    private PlayerService playerService;
+
+    @Inject
+    private PlayerRepository playerRepository;
+
+    @Inject
+    private TeamRepository teamRepository;
+
+    @BeforeEach
+    public void before() {
+
+        Team team1 = new Team();
+        team1.setId(1L);
+        team1.setName("Manchester United");
+        teamRepository.save(team1);
+
+        Team team2 = new Team();
+        team2.setId(2L);
+        team2.setName("Atlético de Madrid'");
+        teamRepository.save(team2);
+
+        Player player1 = new Player();
+        player1.setId(1L);
+        player1.setFirstName("Paul");
+        player1.setLastName("Pogba");
+        player1.setTeam(team1);
+        playerRepository.save(player1);
+
+        Player player2 = new Player();
+        player2.setId(2L);
+        player2.setFirstName("Antoine");
+        player2.setLastName("Griezmann");
+        player2.setTeam(team2);
+        playerRepository.save(player2);
+
+    }
+
+    @DisplaySql
+    @Test
+    public void should_find_all_players_with_team_name() {
+
+        List<PlayerWithTeamName> playersWithTeamName = playerService.findPlayersWithTeamName();
+
+        assertThat(playersWithTeamName).hasSize(2);
+
+    }
+
+}
